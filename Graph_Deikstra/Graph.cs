83453a2f8df2df1253e7e83e2c3999635d04b1f3
Smak80 <smak-80@yandex.ru>
2019-11-19 18:52:14 +0300

    Добавьте файлы проекта.

---
+++ Graph_Deikstra/Graph.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_Deikstra
+{
+	class Graph
+	{
+		private Vertex[] v;
+		private decimal[,] e;
+		private string[] path;
+
+		private uint VertexCount
+		{
+			get { return (uint)e.GetLength(0); }
+		}
+
+		public Graph(decimal[,] graph)
+		{
+			Array.Copy(
+				graph, 
+				e, 
+				graph.Length
+				);
+			v = new Vertex[VertexCount];
+			for (uint i = 0; i < VertexCount; i++)
+			{
+				v[i] = new Vertex(/*i+1*/);
+			}
+		}
+
+		private Vertex GetMinV()
+		{
+			uint i = 0;
+			while (v[i].Visited) i++;
+			uint min = i;
+			while (i < VertexCount)
+			{
+				if (v[i].Label < v[min].Label && !v[i].Visited)
+					min = i;
+				i++;
+			}
+
+			return v[min];
+		}
+
+		public void CalcShortestPath(uint from, uint to)
+		{
+			v[from].Label = 0;
+			for (uint i = 0; i < VertexCount - 1; i++)
+			{
+				Vertex currentV = GetMinV();
+			}
+		}
+
+	}
+}
---
+++ Graph_Deikstra/Vertex.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_Deikstra
+{
+	class Vertex
+	{
+		public uint Label { get; set; }
+		public bool Visited { get; set; }
+
+		public Vertex()
+		{
+			Label = uint.MaxValue;
+			Visited = false;
+		}
+	}
+
+	class Edge
+	{
+		public uint From { get; private set; }
+		public uint To { get; private set; }
+		public decimal Value { get; set; }
+
+	}
+}
