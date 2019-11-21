8c624b3162fad6c59546ed3f36229468dbba18f3
Smak80 <smak-80@yandex.ru>
2019-11-21 10:25:58 +0300

    Коррекция класса Vertex.

--- Graph_Deikstra/Vertex.cs
+++ Graph_Deikstra/Vertex.cs
@@ -8,21 +8,43 @@ namespace Graph_Deikstra
 {
 	class Vertex
 	{
-		public uint Label { get; set; }
+		/**
+		 * Номер вершины графа
+		 */
+		public uint Number { get; private set; }
+		/**
+		 * Метка, равная длине кратчайшего пути на текущем шаге алгоритма до данной вершины из начальной 
+		 */
+		public decimal Label { get; set; }
+		/**
+		 * Признак того, что вершина уже рассмотрена
+		 */
 		public bool Visited { get; set; }
+		/**
+		 * Путь к вершине. Содержит последовательность номеров вершин от начальной
+		 * до текущей (на данном шаге алгоритма), обеспечивающих наикратчайший путь.
+		 */
+		public string Path { get; set; }
 
-		public Vertex()
+		/**
+		 * Константа для пометки бесконечно удаленной вершины 
+		 */
+		public static readonly uint INFINITY = uint.MaxValue;
+
+		public Vertex(uint number)
 		{
-			Label = uint.MaxValue;
+			Label = INFINITY;
 			Visited = false;
+			Number = number;
+			Path = Number.ToString();
 		}
 	}
 
-	class Edge
+	/*class Edge
 	{
 		public uint From { get; private set; }
 		public uint To { get; private set; }
 		public decimal Value { get; set; }
 
-	}
+	}*/
 }
